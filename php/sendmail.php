f016cc84dcb363c1d098d30a73ecc4bba5868f33
thanhDatNT <93346020+thanhDatNT@users.noreply.github.com>
2021-12-12 05:12:39 +0700

    edit code php

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once 'phpmailer/Exception.php';
-require_once 'phpmailer/PHPMailer.php';
-require_once 'phpmailer/SMTP.php';
+require_once '/php/Exception.php';
+require_once '/php/PHPMailer.php';
+require_once '/php/SMTP.php';
 
 $mail = new PHPMailer(true);
 
