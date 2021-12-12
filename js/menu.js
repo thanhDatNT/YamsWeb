311556662ee4c514ab404b8a1b1d77377f914416
quynhvtn01 <quynhvtn19411@st.uel.edu.vn>
2021-12-13 02:39:56 +0700

    update menu 2

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,31 @@
 "use strict";
 (function(){
 // Scroll reveal effect for #container
-    
-    function ShowProduct(){
-        const slide= $('#bestseller .slider-product')
-        const products= $$('#bestseller .slider-product .item')
-        const prevBtn= $('#bestseller .indicator .prev')
-        const nextBtn= $('#bestseller .indicator .next')
-        var counter= 0
-        const size= posts[0].clientWidth
-        
-        slide.style.transform= 'translateX('+ 0 +'px)';
-
-        // Button listener
-        nextBtn.addEventListener('click', () =>{
-            if(counter >= posts.length -1) counter =-1;
-            slide.style.transition = "transform 0.4s ease-in"
-            counter++
-            slide.style.transform= 'translateX('+ (-size * counter) +'px)';
-            products[products.length-1].style.opacity= '1'
+    function ScrollEvent(){
+        ScrollReveal({
+            reset:true,
+            distance: '60px',
+            duration: 2500, 
+            delay:400
         })
-
-        prevBtn.addEventListener('click', () =>{
-            if(counter <= 0) return;
-            slide.style.transition = "transform 0.4s ease-in-out"
-            counter--
-            slide.style.transform= 'translateX('+ (-size * counter) +'px)';
-        })
+        ScrollReveal().reveal('.main-title, .title, #offer .left, #order .left', { delay: 400, origin:'left' });
+        ScrollReveal().reveal('#banners .description, #offer .right', { delay: 300, origin:'right' });
+        ScrollReveal().reveal('.container .item .product-item, #review .slider-wrapper .post:not(:last-child)', { delay: 200, origin:'bottom', interval: 200});
+        ScrollReveal().reveal('.hl-box .content-box, #new .text, #offer .flavour-content', { delay: 200, origin:'left'});
+        ScrollReveal().reveal('.hl-box .image-box, #order .center, #new .flavour-content, #offer .text, #view-all, #offer .title', { delay: 200, origin:'right' });
+        ScrollReveal().reveal('#macaron .title, #cheesecake .title, #pudding .title, #tiramisu .title', { delay: 200, origin:'bottom', interval: 200});
     }
-    ShowPost();
+    ScrollEvent();
 })()
 
+const productItem= $$('#bestseller .product-item')
+productItem.forEach(function(product){
+    product.onclick= function(){
+        window.location.href= 'product-detail.html'
+    }
+})
+
+const btnAddtoCart= $('#bestseller .item .product-item .add')
+btnAddtoCart.onclick= function(e){
+    e.preventDefault()
+}
