f672e109e38d3116a770898f84ea4de22d4ab55b
thanhDatNT <93346020+thanhDatNT@users.noreply.github.com>
2021-12-12 12:19:30 +0700

    edit font a,p tag

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,5 +1,7 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 require_once '/php/Exception.php';
 require_once '/php/PHPMailer.php';
