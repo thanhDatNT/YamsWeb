ecd34a302579dcb3aeef6e56bf84fe2a44a7d57e
quynhvtn01 <quynhvtn19411@st.uel.edu.vn>
2021-12-15 17:17:26 +0700

    add view-all page just interface

--- js/view-all.js
+++ js/view-all.js
@@ -11,9 +11,6 @@
         ScrollReveal().reveal('.main-title, .title, #offer .left, #order .left', { delay: 400, origin:'left' });
         ScrollReveal().reveal('#banners .description, #offer .right', { delay: 300, origin:'right' });
         ScrollReveal().reveal('.container .item .product-item', { delay: 200, origin:'bottom', interval: 200});
-        ScrollReveal().reveal('.hl-box .content-box, #new .text, #offer .flavour-content', { delay: 200, origin:'left'});
-        ScrollReveal().reveal('.hl-box .image-box, #order .center, #new .flavour-content, #offer .text, #view-all, #offer .title', { delay: 200, origin:'right' });
-        ScrollReveal().reveal('#macaron .title, #cheesecake .title, #pudding .title, #tiramisu .title', { delay: 200, origin:'bottom', interval: 200});
     }
     ScrollEvent();
 })()
