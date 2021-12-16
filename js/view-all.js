e45a49ead6e6816e1d641ad6a3f7615200fae69e
quynhvtn01 <quynhvtn19411@st.uel.edu.vn>
2021-12-17 01:42:31 +0700

    update view all menu

--- js/view-all.js
+++ js/view-all.js
@@ -8,12 +8,12 @@
             duration: 2500, 
             delay:400
         })
-        ScrollReveal().reveal('.main-title, .title, #offer .left, #order .left', { delay: 400, origin:'left' });
-        ScrollReveal().reveal('#banners .description, #offer .right', { delay: 300, origin:'right' });
-        ScrollReveal().reveal('.container .item .product-item', { delay: 200, origin:'bottom', interval: 200});
-        ScrollReveal().reveal('.hl-box .content-box, #new .text, #offer .flavour-content', { delay: 200, origin:'left'});
-        ScrollReveal().reveal('.hl-box .image-box, #order .center, #new .flavour-content, #offer .text, #view-all, #offer .title', { delay: 200, origin:'right' });
-        ScrollReveal().reveal('#macaron .title, #cheesecake .title, #pudding .title, #tiramisu .title', { delay: 200, origin:'bottom', interval: 200});
+        ScrollReveal().reveal('.main-title, .title, #offer .left, #order .left', { delay: 50, origin:'left' });
+        ScrollReveal().reveal('#banners .description, #offer .right', { delay: 50, origin:'right' });
+        ScrollReveal().reveal('.container .item .product-item', { delay: 50, origin:'bottom', interval: 50});
+        ScrollReveal().reveal('.hl-box .content-box, #new .text, #offer .flavour-content', { delay: 50, origin:'left'});
+        ScrollReveal().reveal('.hl-box .image-box, #order .center, #new .flavour-content, #offer .text, #view-all, #offer .title, #box .text, #box .content .title', { delay: 50, origin:'right' });
+        ScrollReveal().reveal('#macaron .title, #cheesecake .title, #pudding .title, #tiramisu .title', { delay: 50, origin:'bottom', interval: 100});
     }
     ScrollEvent();
 })()
