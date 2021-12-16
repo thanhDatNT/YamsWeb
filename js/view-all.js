368bcd6f82f947539ef4f03906cc7559fd8252cb
quynhvtn01 <quynhvtn19411@st.uel.edu.vn>
2021-12-16 16:27:11 +0700

    update view all

--- js/view-all.js
+++ js/view-all.js
@@ -11,18 +11,21 @@
         ScrollReveal().reveal('.main-title, .title, #offer .left, #order .left', { delay: 400, origin:'left' });
         ScrollReveal().reveal('#banners .description, #offer .right', { delay: 300, origin:'right' });
         ScrollReveal().reveal('.container .item .product-item', { delay: 200, origin:'bottom', interval: 200});
+        ScrollReveal().reveal('.hl-box .content-box, #new .text, #offer .flavour-content', { delay: 200, origin:'left'});
+        ScrollReveal().reveal('.hl-box .image-box, #order .center, #new .flavour-content, #offer .text, #view-all, #offer .title', { delay: 200, origin:'right' });
+        ScrollReveal().reveal('#macaron .title, #cheesecake .title, #pudding .title, #tiramisu .title', { delay: 200, origin:'bottom', interval: 200});
     }
     ScrollEvent();
 })()
 
-const productItem= $$('#bestseller .product-item')
+const productItem= $$('#all-product .product-item')
 productItem.forEach(function(product){
     product.onclick= function(){
         window.location.href= 'product-detail.html'
     }
 })
 
-const btnAddtoCart= $('#bestseller .item .product-item .add')
+const btnAddtoCart= $('#all-product .item .product-item .add')
 btnAddtoCart.onclick= function(e){
     e.preventDefault()
 }
